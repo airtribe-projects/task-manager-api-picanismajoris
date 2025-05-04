512fbff37749410efd3c585afad236577c3e2529
Amrit Raj <raj.amrit.works@gmail.com>
2025-05-04 15:26:15 +0530

    Set up basic project structure with Express.js server and initial task manager API

---
+++ README.md
@@ -0,0 +1,24 @@
+# Task Manager API
+
+this is my first NodeJS API project for Airtribe!
+
+## how to use
+
+1. clone the repo
+2. Run npm install
+3. Start the server: node app.js
+4. Open browser to http://localhost:3000
+
+## API Endpoints
+
+The api has these endpoints:
+
+- GET / - welcome page
+- GET /tasks - get all tasks
+
+more endpoints coming soon...
+
+## technologies Used
+- NodeJS
+- Express
+- json file for storage
--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const fs = require('fs');
+const path = require("path");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Reading tasks from file
+function readTasks() {
+    try {
+      const tasksData = fs.readFileSync(path.join(__dirname, 'task.json'));
+        return JSON.parse(tasksData);
+    } catch (error) {
+        console.error('Error reading tasks:', error);
+        return { tasks: [] };
+    }
+}
+
+// Writing tasks to file
+function writeTasks(tasks) {
+  try {
+    fs.writeFileSync(
+      path.join(__dirname, 'task.json'),
+      JSON.stringify(tasks, null, 2)
+    );
+  } catch (err) {
+    console.error('Error writing tasks:', err);
+  }
+}
+
+// Welcome route
+app.get('/', (req, res)=>{
+  res.send('Welcome to the Task Manager API!');
+});
+
+// Get all tasks
+app.get('/tasks', function(req, res) {
+    const data = readTasks();
+    res.json(data.tasks || []);
+});
 
 app.listen(port, (err) => {
     if (err) {
@@ -11,7 +47,5 @@ app.listen(port, (err) => {
     }
     console.log(`Server is listening on ${port}`);
 });
-
-
 
 module.exports = app;
